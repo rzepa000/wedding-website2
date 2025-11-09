e0e7f6bc1bee1ede193f9b64a1400760112efeb8
Jakub Rzepinski <100721114+rzepa000@users.noreply.github.com>
2025-11-09 10:44:34 +0000

    change

--- js/scripts.js
+++ js/scripts.js
@@ -212,13 +212,13 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a moment!</strong> We are saving your details.'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is wrong.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwlRmOkfKN8heQ9Esg7dkT93nj2px-WPj4RXx6QnHaQf9RfRH9Q_oMAw-Z-g78y0rG2jQ/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
